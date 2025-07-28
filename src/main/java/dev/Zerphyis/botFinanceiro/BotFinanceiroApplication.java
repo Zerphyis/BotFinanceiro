f99de57b1cf0ac2826da71f0063b000ee3938084
Otavio <estudosprogotavio@gmail.com>
2025-07-28 09:47:04 -0300

    feat:commit inicial

---
+++ src/main/java/dev/Zerphyis/botFinanceiro/BotFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.botFinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Zerphyis/botFinanceiro/BotFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Zerphyis.botFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
