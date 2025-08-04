c59a2ca66ab093f21ef44f8fbcadd77b8f5b019b
Otavio <estudosprogotavio@gmail.com>
2025-08-04 14:14:29 -0300

    refactor:deletando ChatResponse

--- src/main/java/dev/Zerphyis/botFinanceiro/application/chat/ChatResponse.java
+++
@@ -1,7 +0,0 @@
-package dev.Zerphyis.botFinanceiro.application.chat;
-
-public record ChatResponse(String reply, byte[] chartImage) {
-    public ChatResponse(String reply) {
-        this(reply, null);
-    }
-}
