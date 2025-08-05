8166bdc33d1a82a26c22ff85749ced40789130c0
Otavio <estudosprogotavio@gmail.com>
2025-08-05 14:02:05 -0300

    refactor:removendo RequestExpense

--- src/main/java/dev/Zerphyis/botFinanceiro/model/expense/RequestExpense.java
+++
@@ -1,7 +0,0 @@
-package dev.Zerphyis.botFinanceiro.model.expense;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-public record RequestExpense(BigDecimal amount , String category, LocalDate date) {
-}
