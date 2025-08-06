d65de3e21cd63bcf01c31456b745e168cb20af91
Otavio <estudosprogotavio@gmail.com>
2025-08-06 11:45:05 -0300

    feat:adicionando metodos do UserDetails em Usuario

--- src/main/java/dev/Zerphyis/botFinanceiro/model/usuarios/Usuario.java
+++ src/main/java/dev/Zerphyis/botFinanceiro/model/usuarios/Usuario.java
@@ -7,6 +7,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,7 +19,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "Usuario")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,4 +31,23 @@ public class Usuario {
     private Double mediaSalarial;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return String.valueOf(mediaSalarial);
+    }
+
+    @Override
+    public String getUsername() {
+        return nome;
+    }
+
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
 }
